678544edf91a6d81adeb42631233b383d5b8fa82
joao thomaz <joaothomazlemos@gmail.com>
2023-11-05 00:32:06 -0300

    exampels

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -10,7 +10,7 @@ from PIL import Image
 from torch.utils.data import DataLoader
 import glob
 import os
-
+import gradio as gr #interface
 
 
 
@@ -90,24 +90,89 @@ def main(image_folder, single_image=False, image_name=None):
     else:
 
     #create a grid for the predictions for all the images in the example folder if it is the images in a folder to predict
-        for i in range(0, len(glob.glob('src/data/examples/*.jpg'))):
+        for i in range(0, len(glob.glob('src/data/examples/*.jpg'))-1):
             img = Image.open(glob.glob('src/data/examples/*.jpg')[i])
             #transform image
-            img = test_transform(img)
-            img = torch.unsqueeze(img, 0)
+            input = test_transform(img)
+            input = torch.unsqueeze(input, 0)
             #predict
-            prediction = model(img.to(device))
+            prediction = model(input.to(device))
             prediction = threshold_prediction(prediction)
             plt.subplot(2, 2, i+1)
             plt.title('Prediction: {}'.format(prediction))
             plt.imshow(img)
         plt.show()
 
+def gr_main(img):
+    """
+    Predicts whether a book page is being flipped or not, based on an input image.
 
+    Args:
+        
+
+    Returns:
+        None
+    """
+    #set device avaible
+    device = get_device()
+    
+    model = torch.load('C:\\Users\\joaot\\1 notebooks\\APZIVA\\docReader_project4\\flipping-book-detection\\src\\data\\models\\MobileNetV2\\model.pth')
+    #load dictionary of model state weitghts
+    model.load_state_dict(torch.load('C:\\Users\\joaot\\1 notebooks\\APZIVA\\docReader_project4\\flipping-book-detection\\src\\data\\models\\MobileNetV2\\checkpoint.pt'))
+    #set model to evaluation mode
+    model.eval()
+
+    #set model to device
+    model.to(device)
+    #getting the class of the pred
+
+
+    #plot the image with predicted label
+    #0 = flipping the page
+    #1 = not flipping the page
+
+    #img = Image.open(img)
+    
+    #transform image
+    input = test_transform(img)
+    input = torch.unsqueeze(input, 0)
+    #predict
+    prediction = model(input.to(device))
+    prediction = threshold_prediction(prediction)
+    print('Prediction: {}'.format(prediction))
+    return 'Prediction: {}'.format(prediction)
+
+
+
+
+demo = gr.Interface(
+    fn=gr_main,
+    inputs=gr.Image(type="pil"),
+    outputs="text",
+    flagging_options=["correct", "incorrect"],
+    examples=[
+        os.path.join(os.path.abspath(''), "flipping-book-detection/src/models/examples/0001_000000002.jpg"),
+        os.path.join(os.path.abspath(''), "flipping-book-detection/src/models/examples/0002_000000017.jpg"),
+        os.path.join(os.path.abspath(''), "flipping-book-detection/src/models/examples/0001_000000009.jpg"),
+        os.path.join(os.path.abspath(''), "flipping-book-detection/src/models/examples/0001_000000014.jpg"),
+        os.path.join(os.path.abspath(''), "flipping-book-detection/src/models/examples/0001_000000020.jpg"),
+        os.path.join(os.path.abspath(''), "flipping-book-detection/src/models/examples/0001_000000024.jpg"),
+        os.path.join(os.path.abspath(''), "flipping-book-detection/src/models/examples/0002_000000012.jpg"),
+        os.path.join(os.path.abspath(''), "flipping-book-detection/src/models/examples/0002_000000013.jpg"),
+        os.path.join(os.path.abspath(''), "flipping-book-detection/src/models/examples/0002_000000015.jpg"),
+        os.path.join(os.path.abspath(''), "flipping-book-detection/src/models/examples/0002_000000017.jpg"),
+        
+    ],
+)
 
 #an image name: 0001_000000002.jpg
 
 if __name__ == '__main__':
-    main(image_folder='src/data/examples/', single_image=False, image_name=None)
+    demo.launch(show_api=True, share=True)
+    #main(image_folder='src/data/examples/', single_image=False, image_name=None)
+
+    #single images
     #main(image_folder='src/data/examples/', single_image=True, image_name='0001_000000002.jpg')
+    #main(image_folder='src/data/examples/', single_image=True, image_name='0002_000000017.jpg') e esse3
+
 
